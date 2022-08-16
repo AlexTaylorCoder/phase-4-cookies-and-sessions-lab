26ade85d6f270be3235af3958ffee02a3142dc1d
alextaylor <alextaylor515@gmail.com>
2022-08-16 11:02:25 -0400

    commit tree 7252428b424886602b3cc2cdfcbbeb90560f5837

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,12 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    if countsession <= 3
+      article = Article.find(params[:id])
+      render json: article
+    else
+      render json: {error: "Maximum pageview limit reached"}, status: 401
+    end
   end
 
   private
@@ -16,5 +20,9 @@ class ArticlesController < ApplicationController
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
   end
-
+  def countsession
+    session[:page_views] ||= 0 
+    session[:page_views] += 1
+    session[:page_views]
+  end
 end
